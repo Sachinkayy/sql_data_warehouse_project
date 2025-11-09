cf6c2b46b8074b644d8e5d483e5214a3e49ec6af
SACHIN KHAJURIA <sachin.khajuria@autodesk.com>
2025-11-09 16:02:49 +0530

    adding the script of database initialization

---
+++ scripts/databases_initialization.sql
@@ -0,0 +1,35 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a 3 new databases named as Bronze, Silver, Gold after checking if it already exists. 
+    If the database exists, it is dropped and recreated. 
+    Note: In Mysql, we dont have a concept of schema and schema is just another name for a DATABASE
+	
+WARNING:
+    Running this script will drop the entire 'Database' if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+
+
+
+
+-- In MySql we dont have a schema, schema aka database --
+
+-- >> creating databases << --
+DROP DATABASE if exists bronze;
+CREATE SCHEMA  bronze ;
+
+DROP DATABASE if exists silver;
+CREATE DATABASE silver;
+
+DROP DATABASE if exists gold;
+CREATE SCHEMA gold;
+
+
+
+
+
